c70dd517d4a55b1a2289a1f4a35a2c1ab4026b88
pavel <gritsatsuev.pavel@gmail.com>
2023-05-01 05:42:50 +0400

    add bootstrap js

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -26,6 +26,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('register') }}">Зарегистрироваться</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register-manager') }}">Зарегистрировать менеджера</a>
+                    </li>
                 @endif
             </ul>
         </div>
