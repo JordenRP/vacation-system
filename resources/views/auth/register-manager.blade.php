b8740c008589699e4dd29b4821a098bd22360ce3
pavel <gritsatsuev.pavel@gmail.com>
2023-05-01 05:50:25 +0400

    add error block for register manager template

--- resources/views/auth/register-manager.blade.php
+++ resources/views/auth/register-manager.blade.php
@@ -7,14 +7,23 @@
         <div class="mb-3">
             <label for="name" class="form-label">Имя</label>
             <input type="text" class="form-control" id="name" name="name" required>
+            @error('name')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
             <input type="email" class="form-control" id="email" name="email" required>
+            @error('email')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Пароль</label>
             <input type="password" class="form-control" id="password" name="password" required>
+            @error('password')
+            <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="password_confirmation" class="form-label">Повторите пароль</label>
